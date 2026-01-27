dca643b4997a3a016b044de4fb9fcb23beed9741
teklu <teklufa@gmail.com>
2026-01-27 15:57:04 +0300

    initialize many-to-many-lab

---
+++ src/main/java/com/example/spring_jpa_many_to_many_lab/SpringJpaManyToManyLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_many_to_many_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaManyToManyLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaManyToManyLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_jpa_many_to_many_lab/SpringJpaManyToManyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_many_to_many_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaManyToManyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
