33a35ed088dd47013f5386cdecf4f57e29e541b7
teklu <teklufa@gmail.com>
2026-01-28 10:18:55 +0300

    define the two entities with many to many relation

---
+++ src/main/java/com/example/spring_jpa_many_to_many_lab/course/entity/Course.java
@@ -0,0 +1,28 @@
+package com.example.spring_jpa_many_to_many_lab.course.entity;
+
+import com.example.spring_jpa_many_to_many_lab.student.entity.Student;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "courses")
+    private Set<Student> students;
+}
---
+++ src/main/java/com/example/spring_jpa_many_to_many_lab/student/entity/Student.java
@@ -0,0 +1,32 @@
+package com.example.spring_jpa_many_to_many_lab.student.entity;
+
+import com.example.spring_jpa_many_to_many_lab.course.entity.Course;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> courses;
+}
